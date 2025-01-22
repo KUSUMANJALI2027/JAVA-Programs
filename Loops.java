3427e3403efeb16daa35ae528f44099289c622d7
VEMULA KUSUMANJALI <kusumanjalivemula2020@gmail.com>
2025-01-22 22:12:48 +0530

    Update Loops.java

--- Loops.java
+++ Loops.java
@@ -4,8 +4,7 @@ public class Loops {
         // For loop to print numbers from 1 to 5
         for (int i = 1; i <= 5; i++) {
             System.out.println(i);
-        }
-
+        }
         System.out.println("While Loop:");
         // While loop to print numbers from 1 to 5
         int j = 1;
