3ff7ecad3d34843c11b3a58a11fa506b24abb461
VEMULA KUSUMANJALI <kusumanjalivemula2020@gmail.com>
2025-01-28 21:26:15 +0530

    Add files via upload

---
+++ MatrixOperations.java
@@ -0,0 +1,23 @@
+public class MatrixOperations {
+    public static void main(String[] args) {
+        int[][] matrixA = {{1, 2}, {3, 4}};
+        int[][] matrixB = {{5, 6}, {7, 8}};
+        int[][] result = new int[2][2];
+
+        // Matrix Addition
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                result[i][j] = matrixA[i][j] + matrixB[i][j];
+            }
+        }
+
+        // Display result of Matrix Addition
+        System.out.println("Result of Matrix Addition:");
+        for (int[] row : result) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+}
